952ad5e103b88056cefaabf506d1a63ae628f338
zixs <satriyahnd27@gmail.com>
2025-11-05 14:09:59 +0700

    one

---
+++ database/migrations/2025_11_05_062538_create_produks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_kategori')->references('id')->on('kategoris');
+            $table->string('nama');
+            $table->integer('harga');
+            $table->integer('stock');
+            $table->text('deskripsi');
+            $table->date('tanggal_upload');
+            $table->foreignId('id_toko')->references('id')->on('tokos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produks');
+    }
+};
